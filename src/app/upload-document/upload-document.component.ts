2b4f65d3f86010171956b803d8ed4b6986e7f5b8
Chidimma Nwanya <chidynwanya11@gmail.com>
2020-07-30 10:11:05 +0100

    reverting to older version

--- src/app/upload-document/upload-document.component.ts
+++ src/app/upload-document/upload-document.component.ts
@@ -35,26 +35,15 @@ export class UploadDocumentComponent implements OnInit {
    
     reader.readAsText(file);
     reader.onload = e => {
-      console.log('e', e)
       const csv = <string>reader.result;
       this.papa.parse(csv, {
         delimiter: ",",
         header: true,
         skipEmptyLines: true,
-        beforeFirstChunk: function( chunk ) {
-          var rows = chunk.split( /\r\n|\r|\n/ );
-          var headings = rows[0].split( ',' );
-          headings[1] = 'Phone Number';
-          headings[5] = 'Date of Birth';
-          headings[8] = 'Call Allowance';
-          headings[9] = 'Transport Allowance';
-          rows[0] = headings.join();
-          return rows.join( '\n' );
-      },
         complete: (result) => {
           let data = result.data[0];
-          if (('Fullname' in data) && ('Phone Number' in data) && ('Address' in data) && ('State' in data) && ('LGA' in data) &&
-            ('Date of Birth' in data) && ('Salary' in data) && ('Gender' in data) && ('Call Allowance' in data) && ('Transport Allowance' in data)) {
+          if (('Fullname' in data) && ('PhoneNumber' in data) && ('Address' in data) && ('State' in data) && ('LGA' in data) &&
+            ('DateofBirth' in data) && ('Salary' in data) && ('Gender' in data) && ('CallAllowance' in data) && ('TransportAllowance' in data)) {
             this.toastr.success('You have Uploaded the CSV file Sucessfully', 'Successful Upload');
             this.showTable = true;
             this.tableDatas = result.data
@@ -81,20 +70,10 @@ export class UploadDocumentComponent implements OnInit {
       this.papa.parse(csv, {
         header: true,
         skipEmptyLines: true,
-        beforeFirstChunk: function( chunk ) {
-          var rows = chunk.split( /\r\n|\r|\n/ );
-          var headings = rows[0].split( ',' );
-          headings[1] = 'Phone Number';
-          headings[5] = 'Date of Birth';
-          headings[8] = 'Call Allowance';
-          headings[9] = 'Transport Allowance';
-          rows[0] = headings.join();
-          return rows.join( '\n' );
-      },
         complete: (result) => {
           let data = result.data[0];
-          if (('Fullname' in data) && ('Phone Number' in data) && ('Address' in data) && ('State' in data) && ('LGA' in data) &&
-          ('Date of Birth' in data) && ('Salary' in data) && ('Gender' in data) && ('Call Allowance' in data) && ('Transport Allowance' in data)) {
+          if (('Fullname' in data) && ('PhoneNumber' in data) && ('Address' in data) && ('State' in data) && ('LGA' in data) &&
+          ('DateofBirth' in data) && ('Salary' in data) && ('Gender' in data) && ('CallAllowance' in data) && ('TransportAllowance' in data)) {
             this.toastr.success('You have Uploaded the CSV file Sucessfully', 'Successful Upload');
             this.showTable = true;
             this.tableDatas = result.data
